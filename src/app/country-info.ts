6d27f560b372445e25c6f66d7d675d6c35ff26a5
Kimberly Nguyen <kngu376@wgu.edu>
2023-11-06 16:21:24 -0600

    Auto-generated commit [ci skip]

--- src/app/country-info.ts
+++
@@ -1,9 +0,0 @@
-export interface CountryInfo {
-    'id': string,
-    'name': string,
-    'capitalCity': string,
-    'region': string,
-    'incomeLevel': string,
-    'longitude': string,
-    'latitude': string
-}
